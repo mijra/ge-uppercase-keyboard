fd19a7699c9f4c2359a4478dccb55663baac755b
nikoloza <nika.tomadze@gmail.com>
2016-06-21 10:51:19 +0200

    init

---
+++ data.js
@@ -0,0 +1,410 @@
+module.exports = {
+  KeyQ: {
+    printable: true,
+    keyCode: 81,
+    Default: 'ქ',
+    Shift: '',
+    CapsLock: 'Ⴕ',
+    Shift_CapsLock: '',
+    Alt: '',
+    Alt_Shift: ''
+  },
+  KeyW: {
+    printable: true,
+    keyCode: 87,
+    Default: 'წ',
+    Shift: 'ჭ',
+    CapsLock: 'Ⴜ',
+    Shift_CapsLock: 'Ⴝ',
+    Alt: '∑',
+    Alt_Shift: '„'
+  },
+  KeyE: {
+    printable: true,
+    keyCode: 69,
+    Default: 'ე',
+    Shift: '',
+    CapsLock: 'Ⴄ',
+    Shift_CapsLock: '',
+    Alt: '´',
+    Alt_Shift: '´'
+  },
+  KeyR: {
+    printable: true,
+    keyCode: 82,
+    Default: 'რ',
+    Shift: 'ღ',
+    CapsLock: 'Ⴐ',
+    Shift_CapsLock: 'Ⴖ',
+    Alt: '®',
+    Alt_Shift: '‰'
+  },
+  KeyT: {
+    printable: true,
+    keyCode: 84,
+    Default: 'ტ',
+    Shift: 'თ',
+    CapsLock: 'Ⴒ',
+    Shift_CapsLock: 'Ⴇ',
+    Alt: '†',
+    Alt_Shift: 'ˇ'
+  },
+  KeyY: {
+    printable: true,
+    keyCode: 89,
+    Default: 'ყ',
+    Shift: '',
+    CapsLock: 'Ⴗ',
+    Shift_CapsLock: '',
+    Alt: '¥',
+    Alt_Shift: 'Á'
+  },
+  KeyU: {
+    printable: true,
+    keyCode: 85,
+    Default: 'უ',
+    Shift: '',
+    CapsLock: 'Ⴓ',
+    Shift_CapsLock: '',
+    Alt: '',
+    Alt_Shift: ''
+  },
+  KeyI: {
+    printable: true,
+    keyCode: 73,
+    Default: 'ი',
+    Shift: '',
+    CapsLock: 'Ⴈ',
+    Shift_CapsLock: '',
+    Alt: 'ˆ',
+    Alt_Shift: 'ˆ'
+  },
+  KeyO: {
+    printable: true,
+    keyCode: 79,
+    Default: 'ო',
+    Shift: '`',
+    CapsLock: 'Ⴍ',
+    Shift_CapsLock: '',
+    Alt: 'ø',
+    Alt_Shift: 'Ø'
+  },
+  KeyP: {
+    printable: true,
+    keyCode: 80,
+    Default: 'პ',
+    Shift: '~',
+    CapsLock: 'Ⴎ',
+    Shift_CapsLock: '',
+    Alt: 'π',
+    Alt_Shift: '∏'
+  },
+  KeyA: {
+    printable: true,
+    keyCode: 65,
+    Default: 'ა',
+    Shift: '',
+    CapsLock: 'Ⴀ',
+    Shift_CapsLock: '',
+    Alt: 'å',
+    Alt_Shift: 'Å'
+  },
+  KeyS: {
+    printable: true,
+    keyCode: 83,
+    Default: 'ს',
+    Shift: 'შ',
+    CapsLock: 'Ⴑ',
+    Shift_CapsLock: 'Ⴘ',
+    Alt: 'ß',
+    Alt_Shift: 'Í'
+  },
+  KeyD: {
+    printable: true,
+    keyCode: 68,
+    Default: 'დ',
+    Shift: '',
+    CapsLock: 'Ⴃ',
+    Shift_CapsLock: '',
+    Alt: '∂',
+    Alt_Shift: 'Î'
+  },
+  KeyF: {
+    printable: true,
+    keyCode: 70,
+    Default: 'ფ',
+    Shift: '',
+    CapsLock: 'Ⴔ',
+    Shift_CapsLock: '',
+    Alt: 'ƒ',
+    Alt_Shift: 'Ï'
+  },
+  KeyG: {
+    printable: true,
+    keyCode: 71,
+    Default: 'გ',
+    Shift: '',
+    CapsLock: 'Ⴂ',
+    Shift_CapsLock: '',
+    Alt: '˙',
+    Alt_Shift: '˝'
+  },
+  KeyH: {
+    printable: true,
+    keyCode: 72,
+    Default: 'ჰ',
+    Shift: '',
+    CapsLock: 'Ⴠ',
+    Shift_CapsLock: '',
+    Alt: '∆',
+    Alt_Shift: 'Ó'
+  },
+  KeyJ: {
+    printable: true,
+    keyCode: 74,
+    Default: 'ჯ',
+    Shift: 'ჟ',
+    CapsLock: 'Ⴟ',
+    Shift_CapsLock: 'Ⴏ',
+    Alt: '˚',
+    Alt_Shift: 'Ô'
+  },
+  KeyK: {
+    printable: true,
+    keyCode: 75,
+    Default: 'კ',
+    Shift: '',
+    CapsLock: 'Ⴉ',
+    Shift_CapsLock: '',
+    Alt: '¬',
+    Alt_Shift: ''
+  },
+  KeyL: {
+    printable: true,
+    keyCode: 76,
+    Default: 'ლ',
+    Shift: '',
+    CapsLock: 'Ⴊ',
+    Shift_CapsLock: '',
+    Alt: 'Ω',
+    Alt_Shift: 'Ò'
+  },
+  KeyZ: {
+    printable: true,
+    keyCode: 90,
+    Default: 'ზ',
+    Shift: 'ძ',
+    CapsLock: 'Ⴆ',
+    Shift_CapsLock: '',
+    Alt: '≈',
+    Alt_Shift: '¸'
+  },
+  KeyX: {
+    printable: true,
+    keyCode: 88,
+    Default: 'ხ',
+    Shift: '',
+    CapsLock: 'Ⴞ',
+    Shift_CapsLock: '',
+    Alt: 'ç',
+    Alt_Shift: '˛'
+  },
+  KeyC: {
+    printable: true,
+    keyCode: 67,
+    Default: 'ც',
+    Shift: 'ჩ',
+    CapsLock: 'Ⴚ',
+    Shift_CapsLock: 'Ⴙ',
+    Alt: '√',
+    Alt_Shift: 'Ç'
+  },
+  KeyV: {
+    printable: true,
+    keyCode: 86,
+    Default: 'ვ',
+    Shift: '',
+    CapsLock: 'Ⴅ',
+    Shift_CapsLock: '',
+    Alt: '∫',
+    Alt_Shift: '◊'
+  },
+  KeyB: {
+    printable: true,
+    keyCode: 66,
+    Default: 'ბ',
+    Shift: '',
+    CapsLock: 'Ⴁ',
+    Shift_CapsLock: '',
+    Alt: '˜',
+    Alt_Shift: 'ı'
+  },
+  KeyN: {
+    printable: true,
+    keyCode: 78,
+    Default: 'ნ',
+    Shift: '',
+    CapsLock: 'Ⴌ',
+    Shift_CapsLock: '',
+    Alt: 'µ',
+    Alt_Shift: '˜'
+  },
+  KeyM: {
+    printable: true,
+    keyCode: 77,
+    Default: 'მ',
+    Shift: '',
+    CapsLock: 'Ⴋ',
+    Shift_CapsLock: '',
+    Alt: 'µ',
+    Alt_Shift: 'Â'
+  },
+
+  // digits
+  Digit1: {
+    printable: true,
+    keyCode: 49,
+    Default: '1'
+  },
+  Digit2: {
+    printable: true,
+    keyCode: 50,
+    Default: '2'
+  },
+  Digit3: {
+    printable: true,
+    keyCode: 51,
+    Default: '3'
+  },
+  Digit4: {
+    printable: true,
+    keyCode: 52,
+    Default: '4'
+  },
+  Digit5: {
+    printable: true,
+    keyCode: 53,
+    Default: '5'
+  },
+  Digit6: {
+    printable: true,
+    keyCode: 54,
+    Default: '6'
+  },
+  Digit7: {
+    printable: true,
+    keyCode: 55,
+    Default: '7'
+  },
+  Digit8: {
+    printable: true,
+    keyCode: 56,
+    Default: '8'
+  },
+  Digit9: {
+    printable: true,
+    keyCode: 57,
+    Default: '9'
+  },
+  Digit0: {
+    printable: true,
+    keyCode: 48,
+    Default: '0'
+  },
+
+  // symbols
+  IntlBackslash: {
+    printable: true,
+    keyCode: 192,
+    Default: '§'
+  },
+  Minus: {
+    printable: true,
+    keyCode: 189,
+    Default: '-'
+  },
+  Equal: {
+    printable: true,
+    keyCode: 187,
+    Default: '='
+  },
+  BracketLeft: {
+    printable: true,
+    keyCode: 219,
+    Default: '['
+  },
+  BracketRight: {
+    printable: true,
+    keyCode: 221,
+    Default: ']'
+  },
+  Semicolon: {
+    printable: true,
+    keyCode: 186,
+    Default: ';'
+  },
+  Quote: {
+    printable: true,
+    keyCode: 222,
+    Default: '\''
+  },
+  Backslash: {
+    printable: true,
+    keyCode: 220,
+    Default: '\\'
+  },
+  Backquote: {
+    printable: true,
+    keyCode: 192,
+    Default: '`'
+  },
+  Comma: {
+    printable: true,
+    keyCode: 188,
+    Default: ','
+  },
+  Period: {
+    printable: true,
+    keyCode: 190,
+    Default: '.'
+  },
+  Slash: {
+    printable: true,
+    keyCode: 191,
+    Default: '/'
+  },
+
+  // space keys
+  Tab: {
+    printable: true,
+    keyCode: 9
+  },
+  Enter: {
+    printable: true,
+    keyCode: 13
+  },
+
+  Space: {
+    printable: true,
+    keyCode: 32
+  },
+
+  // helper keys
+  Escape: { keyCode: 27 },
+  Backspace: { keyCode: 8 },
+  CapsLock: { keyCode: 20 },
+  ShiftLeft: { keyCode: 16 },
+  ShiftRight: { keyCode: 16 },
+  ControlLeft: { keyCode: 17 },
+  AltLeft: { keyCode: 18 },
+  OSLeft: { keyCode: 91 },
+  Space: { keyCode: 32 },
+  OSRight: { keyCode: 93 },
+  AltRight: { keyCode: 18 },
+
+  // arrows
+  ArrowLeft: { keyCode: 37 },
+  ArrowDown: { keyCode: 40 },
+  ArrowUp: { keyCode: 38 },
+  ArrowRight: { keyCode: 39  }
+}
---
+++ keyboard/index.js
@@ -0,0 +1,147 @@
+'use strict'
+
+require('./style.css')
+require('./grid.css')
+
+const buttons = document.querySelectorAll('button')
+const textarea = document.querySelector('.textarea')
+const eventName = 'letterReceiver'
+const event = new CustomEvent(eventName)
+
+textarea.addEventListener('keydown', (e) => {
+  if (isPrintable(e)) {
+    e.preventDefault()
+  } else {
+    e.target.focus()
+  }
+})
+
+// send event
+buttons.forEach((b) => b.addEventListener('click', clickEmitter))
+
+function clickEmitter (e) {
+  const button = buttonDOMChecker(e)
+  const code = checkButtonCode(button)
+  e.code = code
+  letterSender(e)
+}
+
+
+
+function buttonDOMChecker (e) {
+  const target = e.target
+  const button = target.nodeName === 'BUTTON'
+    ? target
+    : target.parentNode.nodeName === 'BUTTON'
+      ? target.parentNode
+      : new Error ('we cant find button in', target)
+  return button
+}
+
+function checkButtonCode (target) {
+  return target.dataset && target.dataset.code
+}
+
+document.addEventListener('keydown', letterSender)
+document.addEventListener('keyup', buttonDisactivator)
+
+function letterSender (e) {
+  buttonActivator(e)
+
+  const code = e.code
+  setMode(code)
+
+  if (isPrintable(e)) {
+    e.preventDefault()
+    event.code = code
+    textarea.dispatchEvent(event)
+  } else {
+    textarea.focus()
+  }
+}
+
+function buttonQuerySelector (e) {
+  const code = e.code
+  return document.querySelector(`[data-code=${code}]`)
+}
+
+function buttonActivator (e) {
+  const button = buttonQuerySelector(e)
+  button.classList.add('active')
+}
+
+function buttonDisactivator (e) {
+  const button = buttonQuerySelector(e)
+  button.classList.remove('active')
+  clearMode(e.code)
+}
+
+// receive event
+textarea.addEventListener(eventName, letterListener)
+
+function letterListener (e) {
+  if (isPrintable(e)) {
+    e.target.innerHTML += getCodeInMode(e.code, getMode())
+  } else {
+    e.target.focus()
+  }
+}
+
+// data
+const codes = require('../data')
+
+function isPrintable (e) {
+  const code = getCodeProperty(e.code)
+  return code.printable === true
+}
+
+function getCodeProperty (property) {
+  return codes[property]
+}
+
+function getCodeInMode (code, mode = 'Default') {
+  if (!getCodeProperty(code))
+    new Error('code is undefined')
+  return getCodeProperty(code)[mode] || ''
+}
+
+let mode
+let modes = {
+  Alt: false,
+  Shift: false,
+  CapsLock: false
+}
+
+function setMode (code) {
+  if (code === 'AltLeft' || code === 'AltRight')
+    modes['Alt'] = true
+  if (code === 'ShiftLeft' || code === 'ShiftRight')
+     modes['Shift'] = true
+  if (code === 'CapsLock')
+    modes['CapsLock'] = true
+  if (code === 'OSLeft' || code === 'OSRight')
+    modes['CMD'] = true
+}
+
+function clearMode (code) {
+  if (code === 'AltLeft' || code === 'AltRight')
+    modes['Alt'] = false
+  if (code === 'ShiftLeft' || code === 'ShiftRight')
+    modes['Shift'] = false
+  if (code === 'CapsLock')
+    modes['CapsLock'] = false
+  if (code === 'OSLeft' || code === 'OSRight')
+    modes['CMD'] = false
+}
+
+function getMode () {
+  let arr = []
+  for (let i in modes) {
+    modes[i] && arr.push(i)
+  }
+  return arr.length > 0 ? arr.join('_') : 'Default'
+}
+
+function isActive () {
+  return false
+}
